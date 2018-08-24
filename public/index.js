19aa3ecf2196c2ef2b77c1943ff0bd1c03e804db
mari <maricrisbonzo@gmail.com>
2018-08-23 21:29:46 -0700

    For my next project

--- public/index.js
+++ public/index.js
@@ -3,48 +3,77 @@ const ziggeoApp = new ZiggeoApi.V2.Application({
 });
 
 let elementToDisplay = document.getElementById('elementToDisplay');
+let msgToDisplay = document.getElementById('warning');
 let tag;
-document.getElementById('addTag').onclick = () => {
-  tag = document.getElementById("mailTo").value;
-  elementToDisplay.innerHTML = `
+
+document.getElementById('login').onclick = () => {
+  username = document.getElementById("username").value;
+  if (username) {
+    fetch("/login", {
+      credentials: 'include',
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username
+      }),
+    }).catch(err => console.log('Login Error: ', err));
+
+    elementToDisplay.innerHTML = `
+      <div class="item">
+  <div class="title">Who do you want to send a video message to?</div>
+  <div class="form-group">
+  <input type="email" class="form-control vim-input" id="mailTo" aria-describedby="emailHelp" placeholder="Please enter their email">
+  </div>
+  <button id="addTag" type="button" class="btn vm-btn">Next</button>
+    </div>`;
+
+    document.getElementById('addTag').onclick = () => {
+      tag = document.getElementById("mailTo").value;
+      elementToDisplay.innerHTML = `
     <div id="item">
     <ziggeorecorder id="recorder-embedding" ziggeo-tags='${tag}' ziggeo-width=640 ziggeo-height=480 ziggeo-theme="minimalist" ziggeo-themecolor="red"></ziggeorecorder>
     <div id="nextAction"></div>
     </div>
     `;
 
-  const element = document.getElementById('recorder-embedding');
-  /*  Needed to use setTimeout to give the recorder time to
-   *  start its video hosting process. Otherwise my app would
-   *  be checking for the processed event before the video has
-   *  even started to upload.
-   */
-  setTimeout(() => {
-    ZiggeoApi.V2.Recorder.findByElement(element).on('processed', () => {
-      fetch("/mail", {
-        credentials: 'include',
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          mailTo: tag
-        }),
-      }).catch(err => console.log('Error: ', err));
+      const element = document.getElementById('recorder-embedding');
+      /*  Needed to use setTimeout to give the recorder time to
+       *  start its video hosting process. Otherwise my app would
+       *  be checking for the processed event before the video has
+       *  even started to upload.
+       */
+      setTimeout(() => {
+        ZiggeoApi.V2.Recorder.findByElement(element).on('processed', () => {
+          fetch("/mail", {
+            credentials: 'include',
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              mailTo: tag
+            }),
+          }).catch(err => console.log('Error: ', err));
 
-      const nextAction = document.getElementById('nextAction');
-      nextAction.innerHTML =`
+          const nextAction = document.getElementById('nextAction');
+          nextAction.innerHTML =`
         <div><button id="next" type="button" class="btn recorder-btn">Next</button></div>
         `;
 
-      document.getElementById('next').onclick = () => {
-        elementToDisplay.innerHTML = `
+          document.getElementById('next').onclick = () => {
+            elementToDisplay.innerHTML = `
         <div class="item">
         <div class="success-title">Yay, you've sent a video to <u>${tag}</u>!</div>
         <div><a href="https://hello.vimgirl.com/" id="next"><button type="button" class="btn vm-btn">Send another one!</button></a></div>
         </div>
         `;
-      };
-    });
-  }, 2000);
-};
+          };
+        });
+      }, 2000);
+    };
+  } else {
+    msgToDisplay.innerHTML = `Please enter a valid username.`;
+  }
+}
